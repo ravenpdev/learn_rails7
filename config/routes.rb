9301ab19661fb4b4d8231582889b453ca9b55e90
RavenP <ravenp.dev@gmail.com>
2023-08-07 19:58:20 +0800

    Add comment to an article functionality

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
 
   # get "articles", to: "articles#index"
   # get "articles/:id", to: "articles#show", as: "article"
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   # Defines the root path route ("/")
   root "pages#home"
